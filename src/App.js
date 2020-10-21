322d13aceee4c5f1d78761ec82e4bd4e68fb4c54
Elbie-em <emoonga1993@gmail.com>
2020-10-21 23:48:26 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
