44c794152c1c364080e6f3adfa3f923a0952c692
Elbie-em <emoonga1993@gmail.com>
2020-10-23 00:19:02 +0200

    Add BookList function component

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-const BookList = () => {
+const BookList = ({booksData}) => {
   return (
     <div>
-      
+      <h1>Book List</h1>
+     
     </div>
   )
 }
 
-export default BookList
+export default (BookList);
