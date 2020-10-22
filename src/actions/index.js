08a1e80f3728bdd1e4e96ce9336bd2f87b684710
Elbie-em <emoonga1993@gmail.com>
2020-10-23 00:39:43 +0200

    Pass data param to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,16 @@
 const CREATE_BOOK= 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-export const createBook = () => {
+export const createBook = (data) => {
   return {
     type: CREATE_BOOK,
+    payload: data,
   }
 }
 
-export const removeBook = () => {
+export const removeBook = (data) => {
   return {
     type: REMOVE_BOOK,
+    payload: data,
   }
 }
