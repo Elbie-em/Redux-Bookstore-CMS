f184bd0ff0886228bdb68a6930b8eefe42b79c9c
Elbie-em <emoonga1993@gmail.com>
2020-10-23 01:26:40 +0200

    Create booksReducer function

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,5 +1,16 @@
 import { CREATE_BOOK, REMOVE_BOOK} from '../actions/actionTypes';
 
-
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return [
+        ...state, action.payload,
+      ];
+    case REMOVE_BOOK:
+      return state.filter(book => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
 
 export default booksReducer;
