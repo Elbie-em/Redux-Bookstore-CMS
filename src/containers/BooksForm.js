4e2e1b4d3309ef3f6cb49fd8ea75fe4eb18df40c
Elbie-em <emoonga1993@gmail.com>
2020-10-23 00:22:32 +0200

    Add UI form

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -3,7 +3,15 @@ import React from 'react'
 const BooksForm = () => {
   const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"];
   return (
-   
+    <form>
+        <input type="text" placeholder="Book Title Here"></input>
+        <select >
+          {categories.map(category => <option value={category} > {category} </option>  )}
+        </select>
+        <button type="submit" value="Submit" >
+          Submit
+        </button>
+    </form>
   )
 }
 
