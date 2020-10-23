152a8ae4d54de9e2ae5592ba4c0a7b7ba84b344f
Elbie-em <emoonga1993@gmail.com>
2020-10-23 02:40:25 +0200

    Import Provider and Create Store, create redux store, wrap App.js in provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import rootReducer from './reducers/index'
+
+const reduxStore = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
