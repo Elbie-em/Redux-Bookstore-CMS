925678be678776fbe35f0433bc5a3f1ada13b6b7
Elbie-em <emoonga1993@gmail.com>
2020-10-24 00:16:05 +0200

    Add handleRemoveBook props and call it onClick of Remove button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+const Book = ({ book, handleRemoveBook }) => (
   <tr>
     <td>{book.title}</td>
     <td>{book.category}</td>
     <td>{book.id}</td>
+    <td><button onClick={ () => handleRemoveBook(book.id) } > Remove Book </button></td>
   </tr>
 );
 
