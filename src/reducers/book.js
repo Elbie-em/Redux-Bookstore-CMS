84e34be422df66d3b3707200b08d09c760047b25
Elbie-em <emoonga1993@gmail.com>
2020-10-23 02:24:12 +0200

    Add initial state

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,5 +1,25 @@
 import { CREATE_BOOK, REMOVE_BOOK} from '../actions/actionTypes';
 
+const book_a = {
+  id: Math.floor(Math.random() * 100) + 1,
+  title: 'Divergent',
+  category: 'Action',
+};
+
+const book_b = {
+  id: Math.floor(Math.random() * 100) + 1,
+  title: 'Maze Runner',
+  category: 'Action',
+};
+
+const book_c = {
+  id: Math.floor(Math.random() * 100) + 1,
+  title: 'Enders Game',
+  category: 'Sci-Fi',
+};
+
+const initialState = [book_a,book_b,book_c];
+
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_BOOK:
