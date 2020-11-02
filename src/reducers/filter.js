11bdd38b35137c3be0fc4978f3ab74f659e8c34e
Elbie-em <emoonga1993@gmail.com>
2020-11-02 22:23:30 +0200

    Add filterReducer

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,2 +1,12 @@
 import { CHANGE_FILTER } from '../actions/actionTypes';
 
+const filterReducer = (state = '', action) => {
+  switch (action.type) {
+    case CHANGE_FILTER:
+      return (action.payload === 'All' ? '' : action.payload);
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
