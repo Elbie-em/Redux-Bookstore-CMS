c1aa82fcb8bfb12cb012b13a37f9993d5bff62c4
Elbie-em <emoonga1993@gmail.com>
2020-11-03 00:28:05 +0200

    Added  filterbooks prop

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const CategoryFilter = () => {
+const CategoryFilter = ({filterBooks}) => {
 
   const categories = ["All", "Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"];
 
   return (
-    <select id="category" name="category" >
+    <select type='text' id="category" name="category" onChange={filterBooks}>
           {categories.map(category => <option key={category}  value={category} > {category} </option>)}
     </select>
   );
