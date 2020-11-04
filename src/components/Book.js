6e7f9fdde5ec43619fefae2301ad87154c8e80eb
Elbie-em <emoonga1993@gmail.com>
2020-11-04 02:53:59 +0200

    Add datacard element to display book details

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book, handleRemoveBook }) => (
-  <tr>
-    <td>{book.title}</td>
-    <td>{book.category}</td>
-    <td>{book.id}</td>
-    <td><button type="submit" onClick={() => handleRemoveBook(book.id)}> Remove Book </button></td>
-  </tr>
-);
+const Book = ({ book, handleRemoveBook }) => {
+  const percentage = Math.floor(Math.random() * (100 - 1)) / 100;
+  const value = percentage * 100;
+
+  return (
+    <div className="p-3">
+      <div className="data-card mx-auto d-flex flex-column p-3 bg-white">
+        <h6 className='text-muted'>{book.category}</h6>
+        <div className="d-flex flex-row w-100">
+          <h2 className="title-font mr-5 mt-1">{book.title}</h2>
+          <div className='w-75 mt-3 progress'>
+  <div className="progress-bar" role="progressbar" style={{flex:percentage}}>{value}%</div>
+          </div>
+        </div>
+        <h5 className='font-weight-lighter text-primary'>Book Id: {book.id}</h5>
+          <p>
+            <span className="font-weight-lighter text-primary btn btn-link">Comments</span>
+            <span className="text-muted font-weight-lighter h4">|</span>
+            <span className="font-weight-lighter text-primary  btn btn-link">Edit</span>
+            <button className="btn btn-primary float-right" type="submit" onClick={() => handleRemoveBook(book.id)}> Remove Book </button>
+          </p>
+      </div>
+    </div>
+  );
+
+};
 
 Book.propTypes = {
   book: PropTypes.shape({
