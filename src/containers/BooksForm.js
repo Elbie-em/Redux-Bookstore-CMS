385c2be76eddd8f8dba417928f122a5fbcd66e93
Elbie-em <emoonga1993@gmail.com>
2020-11-06 02:59:26 +0200

    Rebuild BookForm

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -26,31 +26,20 @@ const BooksForm = ({ createBook }) => {
   };
 
   return (
-    <div className="p-5 w-100">
-      <form className="form-inline border p-2 d-flex align-items-center">
-        <div className="form-group mb-2">
-          <label htmlFor="title" className="h4 ml-5 mr-2">
-            Book Title
-            <input className="form-control ml-3" type="text" id="title" name="title" placeholder="Book Title Here" onChange={handleChange} value={book.title} />
-          </label>
-        </div>
-        <div className="form-group mb-2">
-          <label htmlFor="category" className="h4 ml-5 mr-2">
-            Choose Category
-            <select className="form-control ml-3" type="text" id="category" name="category" onChange={handleChange} value={book.category} required>
-              {categories.map(category => (
-                <option key={categories.indexOf(category)} value={category}>
-                  {' '}
-                  {category}
-                  {' '}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>
-        <button className="btn btn-primary mb-2  ml-5 mr-2 w-25" type="submit" value="Submit" onClick={handleSubmit}>
-          ADD BOOK
-        </button>
+    <div className="form-area mx-auto mb-5">
+      <h3 class="main-font text-muted font-weight-bold">ADD NEW BOOK</h3>
+      <form className="mt-2">
+        <input className="form-el book-title main-font" type="text" id="title" name="title" placeholder="Book title" onChange={handleChange} value={book.title} />
+        <select className="form-el book-category main-font" type="text" id="category" name="category" onChange={handleChange} value={book.category} required>
+          {categories.map(category => (
+            <option key={categories.indexOf(category)} value={category}>
+              {' '}
+              {category}
+              {' '}
+            </option>
+          ))}
+        </select>
+        <button className="form-btn card-el-font font-weight-bold text-white" type="submit" value="Submit" onClick={handleSubmit}>ADD BOOK</button>
       </form>
     </div>
   );
